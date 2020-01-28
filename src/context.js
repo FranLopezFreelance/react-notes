9b4d6a9dae5e18577f979e73ec9ef179bb234058
Francisco López <franlopez.freelance@gmail.com>
2020-01-28 18:17:35 -0300

    agregando color blanco a las opciones de fondo

--- src/context.js
+++ src/context.js
@@ -3,11 +3,10 @@ import React from 'react'
 //Defino el Estado Global de la aplicación
 //notes contiene el array de todasl notas agregadas, editNote contiene los valores de la nota editada
 const NotesContext = React.createContext({
-    notes: [
-        { id: 1, text: 'Hola Mundo!', background: '#EDBBBB' }
-    ],
+    notes: [],
     selectedNote: null,
     colors: [
+        { name: 'white', hex: '#FFFFFF' },
         { name: 'red', hex: '#EDBBBB' },
         { name: 'orange', hex: '#EAD2BF' },
         { name: 'yellow', hex: '#EDECBB' },
